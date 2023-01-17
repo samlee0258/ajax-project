f35d5a5476a3d065d1bc0f93f0f715cf86ae8f8a
Samuel Lee <slee0258@gmail.com>
2023-01-17 07:07:50 +0000

    In main.js create a function to get pokémon data.

--- js/data.js
+++ js/data.js
@@ -1,1 +1,7 @@
 /* exported data */
+
+// var data = {
+//   view: 'home-page',
+//   entries: [],
+//   nextEntryId: 1
+// };
---
+++ js/main.js
@@ -0,0 +1,18 @@
+// var $searchInput = document.querySelector('#home-page');
+
+// $searchInput.addEventListener('submit', function (e) {
+//   console.log(typeof e);
+// });
+
+// function getPokemonData(name) {
+//   var xhr = new XMLHttpRequest();
+//   xhr.open('GET', 'https://pokeapi.co/api/v2/pokemon/' + name);
+//   xhr.responseType = 'json';
+//   xhr.addEventListener('load', function (e) {
+//     console.log(xhr.status);
+//     console.log(xhr.response);
+//   });
+//   xhr.send();
+// }
+
+// getPokemonData('pikachu');
