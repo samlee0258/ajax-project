eb8d7415c8d5f6e0522322a2b2799106292e07eb
Samuel Lee <slee0258@gmail.com>
2023-01-18 00:16:46 +0000

    In main.js add an event listener to the search button: When clicked change the className of the search result page to class=search-result and Change the className to the home search page to hidden.

--- js/data.js
+++ js/data.js
@@ -1,7 +1,7 @@
 /* exported data */
 
-// var data = {
-//   view: 'home-page',
-//   entries: [],
-//   nextEntryId: 1
-// };
+var data = {
+  view: 'home-page',
+  entries: [],
+  nextEntryId: 1
+};
--- js/main.js
+++ js/main.js
@@ -1,19 +1,42 @@
-// var $searchInput = document.querySelector('#home-page');
+var $searchInput = document.querySelector('#search-bar');
+var $searchForm = document.querySelector('form');
+var $homePage = document.querySelector('div[data-view="home-page"]');
+var $searchResult = document.querySelector('div[data-view="search-result"]');
+// var $searchTab = document.querySelector('.search-tab');
 
-// $searchInput.addEventListener('submit', function (e) {
-//   console.log(typeof e);
+$searchForm.addEventListener('submit', function () {
+  event.preventDefault();
+  // console.log($searchInput.value);
+  getPokemonData($searchInput.value);
+  viewSwap('search-result');
+});
+
+// $searchTab.addEventListener('click', function () {
+//   viewSwap('home-page');
+//   $searchInput.value = '';
 // });
 
-// function getPokemonData(name) {
-//   var xhr = new XMLHttpRequest();
-//   xhr.open('GET', 'https://pokeapi.co/api/v2/pokemon/' + name);
-//   xhr.responseType = 'json';
-//   xhr.addEventListener('load', function (e) {
-//     console.log(xhr.status);
-//     console.log(xhr.response);
-//     console.log(xhr.response.stats[0].stat.name);
-//   });
-//   xhr.send();
-// }
+function getPokemonData(name) {
+  event.preventDefault();
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://pokeapi.co/api/v2/pokemon/' + name);
+  xhr.responseType = 'json';
+  xhr.addEventListener('load', function (e) {
+    // console.log(xhr.status);
+    // console.log(xhr.response);
+    // console.log(xhr.response.sprites.other['official-artwork'].front_default);
+    // console.log(xhr.response.stats[0]);
+  });
+  xhr.send();
+}
 
-// getPokemonData('pikachu');
+function viewSwap(dataView) {
+  data.view = dataView;
+  if (data.view === 'home-page') {
+    $homePage.className = 'home-page';
+    $searchResult.className = 'hidden';
+  } else if (data.view === 'search-result') {
+    $homePage.className = 'hidden';
+    $searchResult.className = 'search-result';
+  }
+}
